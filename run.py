b3c6747933341d48506465b64e482f305daec89b
Christian Scheid <christian@scheid.tech>
2025-07-26 13:46:19 +0200

    Add batch_processing_pipeline

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import logging
 from dotenv import load_dotenv
 
 from pipelines.data_preparation_pipeline import data_preparation_pipeline
+from pipelines.batch_processing_pipeline import batch_processing_pipeline
 
 load_dotenv()
 
@@ -9,9 +9,8 @@ load_dotenv()
 def main() -> None:
     config_path = "configs/data_preparation.yaml"
     data_preparation_pipeline.with_options(config_path=config_path)()
+    batch_processing_pipeline.with_options()()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level="INFO")
-    logging.getLogger().setLevel(logging.INFO)
     main()
