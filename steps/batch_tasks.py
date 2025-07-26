0b778bed09d1daf60c56e2ffef47ede1caa79e48
Christian Scheid <christian@scheid.tech>
2025-07-26 13:44:58 +0200

    Enhance batch processing steps: update task status with existing batch ID and add results to dataset from JSON files

--- steps/batch_tasks.py
+++ steps/batch_tasks.py
@@ -4,6 +4,8 @@ from zenml import step
 from zenml.client import Client
 from zenml.logger import get_logger
 from time import sleep
+from datasets import Dataset
+import pandas as pd
 
 from utils.pydantic_models import BatchFileTaskList
 
@@ -26,14 +28,22 @@ def load_batch_task_list(
     return task_list
 
 
-@step(enable_cache=False)
+@step()
 def wait_and_update_batch(
     task_list: BatchFileTaskList,
     wait_seconds: int = 60,
 ) -> BatchFileTaskList:
     for task in task_list.tasks:
-        # TODO: Use batch_id to check if the task already has a batch
-        # if batch_id is None, check by file_id
+        if task.batch_id is not None:
+            logger.info(f"Task {task.file_id} already has a batch ID {task.batch_id}.")
+            batch = openai.batches.retrieve(task.batch_id)
+            task.status = batch.status
+            task.result_file_id = getattr(batch, "output_file_id", None)
+            logger.info(
+                f"Updated task {task.file_id} with existing batch ID {task.batch_id}."
+            )
+            continue
+
         for batch in openai.batches.list():
             if batch.input_file_id == task.file_id:
                 task.status = batch.status
@@ -109,3 +119,54 @@ def download_batch_results(
 
     logger.info(f"Downloaded {len(downloaded_files)} result files.")
     return downloaded_files
+
+
+@step()
+def add_batch_results_to_dataset(
+    result_files: list[Path],
+    pipeline_name: str = "alt_text_data_preparation_pipeline",
+    step_name: str = "load_data",
+    hf_repo_id: str = "scheidti/vqav2-small-alt-text",
+) -> str:
+    logger.info("Adding batch results to dataset.")
+    run = client.get_pipeline(pipeline_name).last_successful_run
+    dataset: Dataset = run.steps[step_name].output.load()
+    alt_text_mapping = {}
+
+    for file_path in result_files:
+        logger.info(f"Adding results from {file_path} to dataset.")
+        data = pd.read_json(file_path, lines=True)
+        for _, row in data.iterrows():
+            status_code = row.get("response").get("status_code")
+            row_id = int(row.get("custom_id").replace("row_", ""))
+
+            if status_code != 200:
+                logger.warning(
+                    f"Skipping row {row_id} due to status code {status_code}."
+                )
+                continue
+
+            alt_text = (
+                row.get("response")
+                .get("body")
+                .get("choices")[0]
+                .get("message")
+                .get("content")
+            )
+
+            if not alt_text:
+                logger.warning(f"No alt text generated for row {row_id}.")
+                continue
+
+            alt_text_mapping[row_id] = alt_text
+
+    def add_alt_text(example, idx):
+        example["alt_text"] = alt_text_mapping.get(idx, "")
+        return example
+
+    dataset = dataset.map(add_alt_text, with_indices=True)
+    logger.info(f"Attempting to push dataset to Hugging Face Hub: {hf_repo_id}")
+    commit_info = dataset.push_to_hub(hf_repo_id)
+    logger.info(f"Dataset pushed to Hugging Face Hub at {commit_info.repo_url}.")
+
+    return commit_info.repo_url.repo_id
