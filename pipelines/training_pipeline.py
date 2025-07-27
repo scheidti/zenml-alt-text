2bfe8e4e5753279fd4d0b4d6c04e6a493b1d2d55
Christian Scheid <christian@scheid.tech>
2025-07-27 09:44:08 +0200

    Implement load_training_data function to create dataset splits for training, validation, and testing

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,7 +1,9 @@
 from zenml import pipeline
 
+from steps.data_loader import load_training_data
+
 
 @pipeline(name="alt_text_training_pipeline")
 def training_pipeline():
+    data = load_training_data()
     # TODO: Implement the training pipeline logic
-    pass
--- steps/data_loader.py
+++ steps/data_loader.py
@@ -1,8 +1,11 @@
 from zenml import step
 from zenml.logger import get_logger
-from datasets import load_dataset, Dataset
+from zenml.client import Client
+from datasets import load_dataset, Dataset, DatasetDict
 
 logger = get_logger(__name__)
+
+client = Client()
 
 
 @step
@@ -31,3 +34,44 @@ def load_data(
 
     logger.info(f"Loaded dataset {dataset} with split {split}.")
     return data
+
+
+@step
+def load_training_data(
+    pipeline_name: str = "alt_text_batch_processing_pipeline",
+    step_name: str = "add_batch_results_to_dataset",
+    test_size: int = 1000,
+    validation_size: int = 1000,
+    seed: int = 42,
+) -> DatasetDict:
+    run = client.get_pipeline(pipeline_name).last_successful_run
+    hf_dataset_id: str = run.steps[step_name].output.load()
+    data: DatasetDict = load_dataset(hf_dataset_id)
+
+    combined_dataset = None
+    for _, split_data in data.items():
+        if combined_dataset is None:
+            combined_dataset = split_data
+        else:
+            combined_dataset = combined_dataset.concatenate(split_data)
+
+    train_val_split = combined_dataset.train_test_split(
+        test_size=test_size, shuffle=True, seed=seed
+    )
+    test_dataset = train_val_split["test"]
+    train_val_dataset = train_val_split["train"]
+
+    train_val_split = train_val_dataset.train_test_split(
+        test_size=validation_size, shuffle=True, seed=seed
+    )
+    train_dataset = train_val_split["train"]
+    validation_dataset = train_val_split["test"]
+
+    result = DatasetDict(
+        {"train": train_dataset, "test": test_dataset, "validation": validation_dataset}
+    )
+
+    logger.info(
+        f"Created splits - Train: {len(train_dataset)}, Test: {len(test_dataset)}, Validation: {len(validation_dataset)}"
+    )
+    return result
