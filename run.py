62e2cfeb60f3390e26a8c6bb3e151cd823df411b
Christian Scheid <christian@scheid.tech>
2025-07-27 09:43:19 +0200

    Add batch processing and training configuration files; update run.py to support pipeline selection

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import click
 from dotenv import load_dotenv
 
 from pipelines.data_preparation_pipeline import data_preparation_pipeline
@@ -7,11 +8,33 @@ from pipelines.training_pipeline import training_pipeline
 load_dotenv()
 
 
-def main() -> None:
-    config_path = "configs/data_preparation.yaml"
-    data_preparation_pipeline.with_options(config_path=config_path)()
-    batch_processing_pipeline.with_options()()
-    training_pipeline.with_options()()
+@click.command()
+@click.option(
+    "--pipeline",
+    type=click.Choice(["data_preparation", "batch_processing", "training"]),
+    required=True,
+    help="Specify the pipeline to run.",
+)
+def main(pipeline: str) -> None:
+    data_preparation_config_path = "configs/data_preparation.yaml"
+    batch_processing_config_path = "configs/batch_processing.yaml"
+    training_config_path = "configs/training.yaml"
+
+    match pipeline:
+        case "data_preparation":
+            data_preparation_pipeline.with_options(
+                config_path=data_preparation_config_path
+            )()
+        case "batch_processing":
+            batch_processing_pipeline.with_options(
+                config_path=batch_processing_config_path
+            )()
+        case "training":
+            training_pipeline.with_options(config_path=training_config_path)()
+        case _:
+            click.echo(
+                "Invalid pipeline specified. Please choose from 'data_preparation', 'batch_processing', or 'training'."
+            )
 
 
 if __name__ == "__main__":
